4b384721d593a00fe4657f78f3362a8ad2dfb6da
krunal <krunalpabari13>
2023-05-19 00:04:41 +0530

    final email done

--- src/components/Foot.js
+++ src/components/Foot.js
@@ -1,5 +1,26 @@
 import React from 'react'
 export default function Foot() {
+  let submit= async (e)=>{
+    // e.preventDefault();
+    const email=document.getElementById('email').value;
+    const phone=document.getElementById('phone').value;
+    const message=document.getElementById('message').value;
+    if(email=='' || phone=='' || message=='')
+    {
+    alert("Please Provide All The Necessary Details.");
+    }
+    else{
+    const response=     fetch("/api/hello",{
+      method: 'POST',
+      headers:{
+        'Content-Type' : 'application/json'
+      },
+      body:JSON.stringify({mail:email,phoneno:phone,text:message}),
+    });
+      alert("Thanks for contacting us. We will contact you soon");
+  }
+
+  }
   return (
     <div><a className="navbar-brand ps-4   " href="#">
     <img
@@ -11,38 +32,38 @@ export default function Foot() {
     />
   </a>
   
-  <div>
+  <form onSubmit={submit}>
         <h1 className="container text-white margin-top  ">LET'S CONNECT</h1>
         <div className="container">
           <div className="row">
             <div className="col-lg-6 col-md-6 col-sm-12 mt-4">
               
-              <input type='text' className='form-control' placeholder='UserName'></input>
+              <input type='text' className='form-control' placeholder='UserName' ></input>
             </div>
 
             <div className="col-lg-6 col-md-6 col-sm-12 mt-4">
               
-              <input type='email' className='form-control' placeholder='Enter Your Email'></input>
+              <input type='email' className='form-control' placeholder='Enter Your Email' id='email'></input>
             </div>
           </div>
 
           <div className="row">
             <div className="col-lg-6 col-md-6 col-sm-12 mt-4">
               
-              <input type='text' className='form-control' placeholder='Entery Your PhoneNo'></input>
+              <input type='text' className='form-control' placeholder='Entery Your PhoneNo' id='phone'></input>
             </div>
 
             <div className="col-lg-6 col-md-6 col-sm-12 mt-4">
               
-              <input type='text' className='form-control' placeholder='Type Your Message'></input>
+              <input type='text' className='form-control' placeholder='Type Your Message' id='message'></input>
             </div>
           </div>
 
           <div className="text-center">
-          <button className="btn  btn-success btn-lg col-6 text-white mt-4 " type="button" style={{margin:'auto',color:'lightgreen',borderRadius:'30px'}}>Submit</button>
+          <input className="btn  btn-success btn-lg col-6 text-white mt-4 " type="submit" style={{margin:'auto',color:'lightgreen',borderRadius:'30px'}}></input>
           </div>
         </div>
-      </div>
+      </form>
   </div>
   )
 }
--- src/pages/api/hello.js
+++ src/pages/api/hello.js
@@ -4,45 +4,38 @@ const readline = require('readline');
 
 
 export default async function handler(req, res) {
-
+    
+  const usermail=req.body.mail;
+  const usertext=req.body.text;
+  const phoneno=req.body.phoneno;
   const transporter = nodemailer.createTransport({
     service: 'gmail',
+    host:'smtp.gmail.com',
+    port:587,
+    secure:true,
     auth: {
       user: 'krunalpabari2012@gmail.com',
-      pass: 'Krunal@@@123'
-    }
+      pass: 'uoshskcstpxcimau',
+    },
   });
   
   // create email options
   const mailOptions = {
-    from: 'krunalpabari2012@gmail.com',
-    to: 'krunalpabari13@gmail.com',
-    subject: 'Node.js Email Test',
-    text: 'hello'
+    from: usermail,
+    to: 'krunalpabari2012@gmail.com',
+    subject: 'User Query',
+    text: usertext+"\nMobile No"+phoneno
   };
   
   // send email
   transporter.sendMail(mailOptions, (error, info) => {
     if (error) {
       console.error(error);
+      res.send("error");
     } else {
       console.log('Email sent: ' + info.response);
+      alert("Thanks for Contacting Us. We Will Contact You Soon");
+      res.send("success");
     }
   });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 }
